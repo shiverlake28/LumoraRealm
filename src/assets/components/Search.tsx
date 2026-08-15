084e29d71c8adb28bdb18f79fd9a8d755f58354a
shiver_lake28 <adityapasari278@gmail.com>
2026-08-15 06:16:38 +0530

    Some minor changes

--- src/assets/components/Search.tsx
+++ src/assets/components/Search.tsx
@@ -1,7 +1,44 @@
 function Search(){
     return(
+        
         <div className="w-full min-h-screen font-space flex flex-col bg-linear-to-b from-[rgb(20,48,54)] via-[rgb(35,45,57)] to-[rgb(58,39,57)]">
-            
+            {/*Searchbar*/}
+            <div>
+                <div></div> //icon
+                <div></div> //input
+            </div>
+            {/*Recent Searches*/}
+            <div>
+            <h2></h2>//Recent Searches
+            <div>
+                <div></div>
+                <div></div>//The Searches
+                <div></div>
+                <div></div>
+            </div>
+             {/*Trending Searches  */}
+            <div>
+                <div></div>
+                <div></div>//The trending searches
+                <div></div>
+                <div></div>
+            </div>
+            {/* Search result */}
+            <div>
+                <h2></h2>//Top Match
+                <div></div>//Top match card + poster
+            </div>
+            {/* More Picks */}
+            <div>
+                <h2></h2>//More Picks
+                <div></div>//More picks card + poster
+            </div>
+
+            {/* Creator details */}
+            <div>
+                //Creator details and info
+            </div>
+            </div>
         </div>
 
     )
