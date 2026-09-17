5e7e4d90111648d0d7863b34fc7b432561b1fb9f
shiver_lake28 <adityapasari278@gmail.com>
2026-09-17 23:00:38 +0530

    Some updates in the code

--- src/assets/components/SearchBox.tsx
+++ src/assets/components/SearchBox.tsx
@@ -3,29 +3,37 @@ import { X } from "lucide-react";
 import { useState } from "react";
 function SearchBox(){
     function HandleSearch(){
-        
+        if (!Search.trim()){
+            return
+        }
+        else{
+            SetRecentSearches((prev) => [Search , ...prev].slice(0,3))
+            SetSearch("")
+        }
+
     }
-    const [RecentSearches,SetRecentSearches] = useState([])
+    const [RecentSearches,SetRecentSearches] = useState<string[]>([])
     const [Search,SetSearch] = useState("")
 
     return(
-        
         <div className="w-full min-h-screen font-space flex flex-col justify-start p-15 pt-10 items-start gap-10 bg-linear-to-b from-[rgb(20,48,54)] via-[rgb(35,45,57)] to-[rgb(58,39,57)]">
             {/*Searchbar*/}
             <div className="w-full h-auto p-2 flex justify-start items-end pb-3 gap-10 bg-[rgb(24,26,21)]/50 border-2 border-gray-300/70 rounded-3xl text-[rgb(226,218,218)]/80 ">
                 <Icon icon="gala:search" width="50" height="50" className=" -scale-x-100 ml-2"/>
-                <div className="flex-1 "><input type="text" onChange={(e)=> SetSearch(e.target.value)} placeholder="Search movies,series,sports,actors..." className="w-full text-2xl placeholder:text-[rgb(139,137,137)] outline-none" /></div>
-                < X className="w-10 h-10 cursor-pointer"/>
+                <div className="flex-1 "><input type="text" value={Search} onChange={(e)=> SetSearch(e.target.value)} onKeyDown={(e)=>{ if (e.key ==="Enter"){HandleSearch()}}} placeholder="Search movies,series,sports,actors..." className="w-full text-2xl placeholder:text-[rgb(139,137,137)] outline-none" /></div>
+                < X className="w-10 h-10 cursor-pointer" onClick={() => SetSearch("")}/>
             </div>
             {/*Recent Searches*/}
-            <div>
+            {RecentSearches.length > 0 &&
+            <div className="space-y-6">
             <h1 className="text-[rgb(235,231,220)]/95 text-lg sm:text-xl">Recent Searches</h1>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div></div>
+            <div className="w-full h-auto p-1 bg-[rgb(24,26,21)]/40 flex gap-10 m text-[rgb(226,218,218)]/80 border-3 border-gray-300/70 space-x-2  rounded-xl text-lg ">
+                {RecentSearches.map((search, index) => (
+            <div className="cursor-pointer px-2  bg-[rgb(24,27,31)]/70 rounded-lg border border-gray-300/20">
+                {search}
+            </div>
+        ))}
+            </div></div>}
              {/*Trending Searches  */}
             <div>
                 <div></div>
